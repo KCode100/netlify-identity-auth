7b6e7289fd130b0d720521aece3b1093ca49313a
Kivi Corn <kivi@Kivis-MBP.lan>
2023-06-22 22:08:59 +0100

    add login button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,11 @@
+'use client'
+
+import AuthContext from "@/stores/authContext";
+import { useContext } from "react";
+
 const Header = () => {
+  const { user, login } = useContext(AuthContext)
+
   return ( 
     <section className="bg-center bg-no-repeat bg-[url('/background.jpg')] bg-gray-700 bg-blend-multiply">
       <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
@@ -7,11 +14,12 @@ const Header = () => {
         <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
           <a href="/secure" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
             Secure Page
-            <svg aria-hidden="true" className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            <svg aria-hidden="true" className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
           </a>
           <a href="https://docs.netlify.com/visitor-access/identity/" target="_blank" className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
             Netlify Docs
           </a>  
+          <button onClick={login}>Login here</button>
         </div>
       </div>
     </section>
